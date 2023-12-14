91260cb1fd1ba61c908f1bead84499623e643d5f
Ismael <Ismael.Tortosa@hotmail.com>
2023-12-14 12:11:27 +0100

    pruebas front

--- app/src/main/java/ismaapp/tortosa/glucoseregister/MainActivity.kt
+++ app/src/main/java/ismaapp/tortosa/glucoseregister/MainActivity.kt
@@ -3,26 +3,33 @@ package ismaapp.tortosa.glucoseregister
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Send
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
-import ismaapp.tortosa.glucoseregister.ui.theme.GlucoseRegisterTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            GlucoseRegisterTheme {
+            MaterialTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    GlucoseMeasurementScreen()
                 }
             }
         }
@@ -30,17 +37,84 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun GlucoseMeasurementScreen() {
+    var glucoseValue by remember { mutableStateOf(0f) }
+    var isMeasurementSuccessful by remember { mutableStateOf(false) }
+    var showMessage by remember { mutableStateOf(false) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Text("Indique la medición:")
+        Spacer(modifier = Modifier.height(8.dp))
+
+        var isError by remember { mutableStateOf(false) }
+        var message by remember { mutableStateOf("") }
+
+        OutlinedTextField(
+            value = glucoseValue.toString(),
+            onValueChange = {
+                isError = false
+                glucoseValue = it.toFloatOrNull() ?: 0f
+            },
+            label = { Text("Ingrese el valor de glucosa") },
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Number
+            ),
+            isError = isError
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Row {
+            Button(
+                onClick = {
+                    // Lógica para registrar la medición aquí
+                    if (glucoseValue >= 80 && glucoseValue <= 120) {
+                        isMeasurementSuccessful = true
+                        showMessage = true
+                        message = "Medición registrada correctamente"
+                    } else {
+                        isMeasurementSuccessful = false
+                        showMessage = true
+                        message = "Medición no registrada correctamente"
+                    }
+                },
+                modifier = Modifier.weight(1f)
+            ) {
+                Icon(Icons.Default.Send, contentDescription = null)
+                Spacer(modifier = Modifier.width(4.dp))
+                Text("Registrar")
+            }
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        if (showMessage) {
+            val icon = if (isMeasurementSuccessful) Icons.Default.Check else Icons.Default.Clear
+            val color = if (isMeasurementSuccessful) Color.Green else Color.Red
+
+            Row(
+                modifier = Modifier
+                    .background(color)
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(icon, contentDescription = null, tint = Color.White)
+                Spacer(modifier = Modifier.width(4.dp))
+                Text(message, color = Color.White)
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    GlucoseRegisterTheme {
-        Greeting("Android")
+fun GlucoseMeasurementScreenPreview() {
+    MaterialTheme {
+        GlucoseMeasurementScreen()
     }
 }
