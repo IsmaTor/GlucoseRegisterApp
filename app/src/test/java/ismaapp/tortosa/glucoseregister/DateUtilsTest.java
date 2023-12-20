d380a7f32ba0950dc88a2f5ea83acd58ed7603ab
Ismael <Ismael.Tortosa@hotmail.com>
2023-12-20 10:52:52 +0100

    pruebas sonar

---
+++ app/src/test/java/ismaapp/tortosa/glucoseregister/DateUtilsTest.java
@@ -0,0 +1,38 @@
+package ismaapp.tortosa.glucoseregister;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+
+public class DateUtilsTest {
+
+    @Test
+    public void testGetCurrentDate2() {
+        Date currentDate = DateUtils.getCurrentDate();
+        assertNotNull(currentDate);
+        // You can add more assertions based on your requirements
+    }
+
+    @Test
+    public void testGetCurrentDate() {
+        try (MockedStatic<DateUtils> mockedStatic = Mockito.mockStatic(DateUtils.class)) {
+            // Mock the static method
+            Date mockDate = new Date();
+            mockedStatic.when(DateUtils::getCurrentDate).thenReturn(mockDate);
+
+            // Test the method that uses getCurrentDate
+            Date currentDate = DateUtils.getCurrentDate();
+            assertEquals(mockDate, currentDate);
+        }
+    }
+    
+}
+
